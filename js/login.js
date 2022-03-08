82369a3898e051ead5409da7841951b68631c29c
rdnanuar <rdjanuar008@gmail.com>
2022-03-08 11:09:44 +0700

    addin_validation

--- js/login.js
+++ js/login.js
@@ -13,14 +13,14 @@ function login() {
     let break_lines = text.split("\r\n")
     // Looping data
     break_lines.forEach(element => {
-    // Memisahkan element [|] di dalam string dan covert menjadi array
-    var pecah_element = element.split("|")
-    // Validasi Login input value user
-    if (pecah_element[0] == nik && pecah_element[1] == username) {
-        window.location.assign("menu.html")
-        alert(`Anda Berhasil login Sebagai ${pecah_element[1]}`)
+        // Memisahkan element [|] di dalam string dan covert menjadi array
+        var pecah_element = element.split("|")
+        // Validasi Login input value user
+        if (pecah_element[0] == nik && pecah_element[1] == username) {
+            window.location.assign("menu.html")
+            alert(`Anda Berhasil login Sebagai ${pecah_element[1]}`)
             }
         })
-        
+        alert("Mohon masukkan data dengan benar!")
     })
 }
