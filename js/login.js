ecc9b4a0af051c1f70177461996d68dca920ecf2
rdnanuar <rdjanuar008@gmail.com>
2022-03-18 20:46:42 +0700

    debuging_login.js

--- js/login.js
+++ js/login.js
@@ -1,26 +1,26 @@
-function login() {
+async function login() {
     // dom js
     const nik = document.getElementById("nik").value
     const username = document.getElementById("username").value
     // Baca Data dari file config
-    fetch("../data/config.txt")
-    .then(response => {
+    const get_data = fetch("../data/config.txt")
+    await get_data.then(response => {
     // Convert response menjadi text
     return response.text()
     })
     .then((text) => {
-    // mengubah text(string) menjadi array dan memisahkan regex new_lines dan cariage return
-    let break_lines = text.split("\r\n")
-    // Looping data
-    break_lines.forEach(element => {
-        // Memisahkan element [|] di dalam string dan covert menjadi array
-        let pecah_element = element.split("|")
-        // Validasi Login input value user
-        if (pecah_element[0] == nik && pecah_element[1] == username) {
-            window.location.assign("menu.html")
-            alert(`Anda Berhasil login Sebagai ${pecah_element[1]}`)
+        // mengubah text(string) menjadi array dan memisahkan regex new_lines dan cariage return
+        let break_lines =  text.split("\r\n")
+        // Looping data
+        break_lines.forEach(element => {
+            // Memisahkan element [|] di dalam string dan covert menjadi array
+            let pecah_element = element.split("|")
+            console.log(pecah_element)
+            // Validasi Login input value user
+            if (pecah_element[0] == nik && pecah_element[1] == username) {
+                window.location.assign("menu.html")
+                alert(`Anda Berhasil login Sebagai ${pecah_element[1]}`)
             }
         })
-        alert("Mohon masukkan data dengan benar!")
     })
 }
