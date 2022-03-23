6432fca016e65eb1849b97bdaf4267d0a081c621
rdnanuar <rdjanuar008@gmail.com>
2022-03-23 16:14:40 +0700

    adding validation

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
-async function login() {
+function login() {
     // dom js
     const nik = document.getElementById("nik").value
     const username = document.getElementById("username").value
     // Baca Data dari file config
     const get_data = fetch("../data/config.txt")
-    await get_data.then(response => {
+    get_data.then(response => {
     // Convert response menjadi text
     return response.text()
     })
@@ -20,6 +20,32 @@ async function login() {
             if (pecah_element[0] == nik && pecah_element[1] == username) {
                 window.location.assign("menu.html")
                 alert(`Anda Berhasil login Sebagai ${pecah_element[1]}`)
+            }
+        })
+    })
+}
+
+function validation() {
+    // dom js
+    const nik = document.getElementById("nik").value
+    const username = document.getElementById("username").value
+    // Baca Data dari file config
+    const get_data = fetch("../data/config.txt")
+    get_data.then(response => {
+    // Convert response menjadi text
+    return response.text()
+    })
+    .then((text) => {
+        // mengubah text(string) menjadi array dan memisahkan regex new_lines dan cariage return
+        let break_lines =  text.split("\r\n")
+        // Looping data
+        break_lines.forEach(element => {
+            // Memisahkan element [|] di dalam string dan covert menjadi array
+            let pecah_element = element.split("|")
+            console.log(pecah_element)
+            // Validasi Login input value user
+            if (pecah_element[0] != nik && pecah_element[1] != username) {
+                alert("NIK atau Username Salah")
             }
         })
     })
